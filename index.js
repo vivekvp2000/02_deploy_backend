7eff0a7af6e375707135bfd1826c6889f679ffaf
vivekvp2000 <vivekpayal999@gmail.com>
2024-11-27 11:15:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome To Chai Or Code" });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ message: "App is working fine..." });
+});
+
+app.listen(port, () => {
+  console.log(`Port is Running on ${port}`);
+});
